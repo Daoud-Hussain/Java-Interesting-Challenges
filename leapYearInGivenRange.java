b7906210c641d6216d20450a8d38d66cb22856d0
Daoud-Hussain <87219816+Daoud-Hussain@users.noreply.github.com>
2021-11-12 19:46:16 +0500

    Update leapYearInGivenRange.java

--- leapYearInGivenRange.java
+++ leapYearInGivenRange.java
@@ -3,15 +3,25 @@ Write a program that displays all the leap years, ten per line, from 101 to 2100
 exactly one space. Also display the number of leap years in this period.
 */
 
-
+import java.util.*;
 public class leapYearInGivenRange{
 	public static void main(String[] args) {
-
+		
 		//Count variable will count the number of leap years b/w 101 and 2100
 		int count = 0;
 
+		//You can uncomment the following code if you want to input the range from user
+		/*
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the starting range: ");
+		int startingRange = input.nextInt();
+		
+		System.out.print("Enter the ending range: ");
+		int endingRange = input.nextInt(); 
+		for (int i=startingRange; i<=endingRange;i++){
+		*/
+
 		//Applying loops in range of 101 and 2100
-		//You can take the inputs from user if you want
 		for (int i=101; i<=2100;i++){
 			
 			//Condition for LEap year
@@ -25,8 +35,7 @@ public class leapYearInGivenRange{
 				}	
 			}
 		}
-
-		//Displying total number of leap years between 101 - 2100
+	//Displying total number of leap years between 101 - 2100
 		System.out.println("Total leap years are: " + count);
 	}
 }
