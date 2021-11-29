c2da3dfadf56c1416cdc88ea35c6231e3fbcf073
Daoud-Hussain <87219816+Daoud-Hussain@users.noreply.github.com>
2021-11-29 11:19:31 +0500

    Update CheckPalindrome.java

--- CheckPalindrome.java
+++ CheckPalindrome.java
@@ -1,7 +1,6 @@
 /*
 Write a program that checks whether a string is a palindrome.A string is a palindrome if it reads the same forward and backward.
-The words mom,  dad and noon for instance, are all palindromes.*/
-/*
+The words mom,  dad and noon for instance, are all palindromes.
 Sample run:
 Enter a string: noon
 noon is a palindrome
