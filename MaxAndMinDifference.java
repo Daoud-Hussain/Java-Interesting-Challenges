90e695b0b90dc6d86b75ed9725833d92bf5e76b1
Daoud-Hussain <87219816+Daoud-Hussain@users.noreply.github.com>
2021-12-30 23:17:54 +0500

    Update MaxAndMinDifference.java

--- MaxAndMinDifference.java
+++ MaxAndMinDifference.java
@@ -5,7 +5,7 @@ that have the maximum and minimum difference among all element pairs.
 */
 
 
-import java.util.Scanner;
+import java.util.*;
 public class MaxAndMinDifference {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
